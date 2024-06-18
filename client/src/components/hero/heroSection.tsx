82cb5ff13f6bc20a25eb26fa7d833a03c3e71acc
Mangalam Raj <mango.26june@gmail.com>
2024-06-18 20:07:14 +0530

    removed from phone

--- client/src/components/hero/heroSection.tsx
+++ client/src/components/hero/heroSection.tsx
@@ -2,12 +2,17 @@ import GlassCard from "../glassComponent/glassCard";
 import "./hero.css";
 import Lottie, { Options } from "react-lottie";
 import animationData from "../../lotties/hero-animation.json";
+
 const HeroSection = () => {
   const defaultOptions: Options = {
     loop: true,
     autoplay: true,
     animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
+
   return (
     <div>
       <div className="relative w-full overflow-x-clip mt-10 md:mt-14">
@@ -20,21 +25,20 @@ const HeroSection = () => {
           />
         </div>
         <div className="absolute -right-[390px] -top-80 -z-50 hidden md:block">
-          <img
-            src="/HeroMesh.svg"
-            alt="gradient-bg"
-            width="match_parent"
-            height="wrap_content"
-          />
+          <img src="/HeroMesh.svg" alt="gradient-bg" width="900" height="900" />
         </div>
       </div>
-      <div className="container  2xl:gap-8 gap-2  2xl:flex-row">
-        <div className="flex ">
+      <div className="container 2xl:gap-8 gap-2 2xl:flex-row">
+        <div className="flex">
           <div className="2xl:text-7xl text-4xl font-semibold font-serif md:w-[60%]">
             Integrated, Whole-Person Care Services For Clinics
           </div>
-          <div className="absolute right-0 left-0 mx-auto sm:w-full sm:max-w-xs md:right-20 overflow-hidden md:overflow-visible">
-            <Lottie options={defaultOptions} height={750} width={750} />
+          <div className="absolute right-0 left-0 mx-auto sm:w-full sm:max-w-xs md:right-20 overflow-hidden hidden md:block md:overflow-visible">
+            <Lottie
+              options={defaultOptions}
+              height={600} // Reduced height
+              width={600} // Reduced width
+            />
           </div>
         </div>
         <div className="flex flex-col justify-between my-auto">
@@ -51,7 +55,6 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-
       {/* <div className="container mt-10">
         <div className="border-gray-200 border-[1px] shadow-sm shadow-blue-500/20 rounded-xl box-shadow">
           <video
