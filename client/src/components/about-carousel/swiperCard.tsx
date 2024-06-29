73cfaec0b8a4001267f8c9733a47fae93d170fed
Mangalam Raj <mango.26june@gmail.com>
2024-06-29 16:20:40 +0530

    gap fix

--- client/src/components/about-carousel/swiperCard.tsx
+++ client/src/components/about-carousel/swiperCard.tsx
@@ -14,7 +14,7 @@ const SwiperCard = ({
   results,
 }: TswiperCard) => {
   return (
-    <div className="flex justify-center flex-col md:flex-row gap-2 p-8">
+    <div className="flex justify-center flex-col md:flex-row gap-4 p-8">
       <div className="border-[2px] p-8 rounded-3xl glass-card text-black max-w-[400px] md:min-h-[450px]">
         <img src={image} className="w-full max-w-[400px]" alt={title} />
         <div className="text-left my-2 md:text-xl text-lg font-semibold">
@@ -24,7 +24,7 @@ const SwiperCard = ({
           {description}
         </div>
       </div>
-      <div className="md:flex-col gap-2 hidden md:flex">
+      <div className="md:flex-col gap-4 hidden md:flex">
         <div className="border-[2px] max-w-[300px] p-8 text-sm text-gray-400 rounded-3xl glass-card min-h-[220px]">
           <div className="text-left my-2 md:text-xl text-lg font-semibold text-black">
             Approach
