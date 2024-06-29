436d7d7f06bee7048d7ca69d9d92d6d133045d4c
Mangalam Raj <mango.26june@gmail.com>
2024-06-29 16:19:32 +0530

    height bug fix

--- client/src/components/about-carousel/swiperCard.tsx
+++ client/src/components/about-carousel/swiperCard.tsx
@@ -15,7 +15,7 @@ const SwiperCard = ({
 }: TswiperCard) => {
   return (
     <div className="flex justify-center flex-col md:flex-row gap-2 p-8">
-      <div className="border-[2px] p-8 rounded-3xl glass-card text-black max-w-[400px]">
+      <div className="border-[2px] p-8 rounded-3xl glass-card text-black max-w-[400px] md:min-h-[450px]">
         <img src={image} className="w-full max-w-[400px]" alt={title} />
         <div className="text-left my-2 md:text-xl text-lg font-semibold">
           {title}
@@ -25,13 +25,13 @@ const SwiperCard = ({
         </div>
       </div>
       <div className="md:flex-col gap-2 hidden md:flex">
-        <div className="border-[2px] max-w-[300px] p-8 text-sm text-gray-400 rounded-3xl glass-card ">
+        <div className="border-[2px] max-w-[300px] p-8 text-sm text-gray-400 rounded-3xl glass-card min-h-[220px]">
           <div className="text-left my-2 md:text-xl text-lg font-semibold text-black">
             Approach
           </div>
           {details}
         </div>
-        <div className="border-[2px] max-w-[300px] p-8 text-sm text-gray-400 rounded-3xl glass-card">
+        <div className="border-[2px] max-w-[300px] p-8 text-sm text-gray-400 rounded-3xl glass-card min-h-[220px]">
           <div className="text-left my-2 md:text-xl text-lg font-semibold text-black">
             Results
           </div>
