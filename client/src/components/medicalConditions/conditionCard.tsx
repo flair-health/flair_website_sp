d369594c7da7ef32c78f226b51013d72ae859383
Mangalam Raj <mango.26june@gmail.com>
2024-07-11 23:08:41 +0530

    changes2

--- client/src/components/medicalConditions/conditionCard.tsx
+++ client/src/components/medicalConditions/conditionCard.tsx
@@ -2,11 +2,17 @@ type Tmedical = {
   name: string;
   image: string;
 };
+
 const ConditionCard = (props: Tmedical) => {
   return (
-    <div className="text-center flex flex-col gap-8  p-10 rounded-3xl">
-      <img src={props.image} className="w-[100px] m-auto"></img>
-      <div className="text-2xl font-semibold ">{props.name}</div>
+    <div className="text-center flex flex-col items-center justify-center gap-12 p-10 relative">
+      <div className="rounded-full h-[160px] w-[160px] bg-[#14a8e1] absolute top-4 bg-opacity-20"></div>
+      <img
+        src={props.image}
+        alt={props.name}
+        className="w-[100px] relative z-10"
+      />
+      <div className="text-2xl font-semibold relative z-10">{props.name}</div>
     </div>
   );
 };
