d68eda296002e50ba1b293558cf68d1a746b6321
Mangalam Raj <mango.26june@gmail.com>
2024-07-12 00:27:06 +0530

    logos

--- client/src/components/collab/collab.tsx
+++ client/src/components/collab/collab.tsx
@@ -1,25 +1,23 @@
 "use client";
 
-import styled, { keyframes, css } from "styled-components";
+import styled from "styled-components";
 
 const MarqueeEffect = () => {
   const row1 = [
     "https://chicagoarthritis.com/wp-content/uploads/2022/08/chicago-arthritis-logo.svg",
     "/UnabridgedMD.webp",
     "/valley.png",
-    "https://chicagoarthritis.com/wp-content/uploads/2022/08/chicago-arthritis-logo.svg",
-    "/UnabridgedMD.webp",
-    "/valley.png",
   ];
+  const row2 = ["/atena.svg", "/cigna.svg", "/united.svg"];
 
   return (
-    <div className="container md:my-20">
+    <div className="container md:my-10">
       <div className="2xl:text-4xl tracking-tight md:text-center m-auto font-semibold text-2xl 2xl:pt-20 pt-10 md:pb-2 pb-2">
-        Our Customers
+        Our Partners
       </div>
-      <div className="md:text-center text-sm font-normal text-gray-500 2xl:text-base 2xl:w-[60%]  m-auto">
-        Flair Health is working with patients from rheumatology private
-        practices across the country.
+      <div className="md:text-center text-sm font-normal text-gray-500 2xl:text-base 2xl:w-[60%] m-auto">
+        Health is working with patients from rheumatology private practices
+        across the country.
       </div>
       <AppContainer>
         <Wrapper>
@@ -31,15 +29,23 @@ const MarqueeEffect = () => {
                 </ImageGroup>
               ))}
             </MarqueeGroup>
+          </Marquee>
+        </Wrapper>
+      </AppContainer>
+      <div className="md:text-center md:pt-20 2xl:text-3xl tracking-tight pb-4  m-auto font-semibold text-2xl">
+        Flair supports patients with the following insurances.
+      </div>
+      <AppContainer>
+        <Wrapper>
+          <Marquee>
             <MarqueeGroup>
-              {row1.map((el, index) => (
-                <ImageGroup key={index + row1.length}>
+              {row2.map((el, index) => (
+                <ImageGroup key={index}>
                   <Image src={el} alt="companies" />
                 </ImageGroup>
               ))}
             </MarqueeGroup>
           </Marquee>
-          <Marquee></Marquee>
         </Wrapper>
       </AppContainer>
     </div>
@@ -59,7 +65,6 @@ const AppContainer = styled.div`
 const Wrapper = styled.div`
   width: 100%;
   height: fit-content;
-
   display: flex;
   align-items: center;
   justify-content: center;
@@ -75,40 +80,19 @@ const Marquee = styled.div`
   width: 1200px;
   overflow: hidden;
   user-select: none;
-
-  mask-image: linear-gradient(
-    to right,
-    hsl(0 0% 0% / 0),
-    hsl(0 0% 0% / 1) 10%,
-    hsl(0 0% 0% / 1) 90%,
-    hsl(0 0% 0% / 0)
-  );
   @media (max-width: 768px) {
     width: 800px;
   }
 `;
 
-const scrollX = keyframes`
-  from {
-    left: translateX(0);
-  }
-  to {
-    transform: translateX(-100%);
-  }
-`;
-
-const common = css`
+const MarqueeGroup = styled.div`
   flex-shrink: 0;
   display: flex;
   align-items: center;
   justify-content: space-around;
   white-space: nowrap;
-  width: 100%;
-  animation: ${scrollX} 30s linear infinite;
-`;
-
-const MarqueeGroup = styled.div`
-  ${common}
+  width: 80%;
+  margin: auto;
 `;
 
 const ImageGroup = styled.div`
@@ -120,13 +104,12 @@ const ImageGroup = styled.div`
 
 const Image = styled.img`
   object-fit: contain;
-  width: 100%;
+  width: 60%;
   height: 100%;
   filter: grayscale(100%);
   &:hover {
     filter: grayscale(0%);
   }
-  /* border: 1px solid black; */
   border-radius: 0.5rem;
   aspect-ratio: 16/9;
   cursor: pointer;
