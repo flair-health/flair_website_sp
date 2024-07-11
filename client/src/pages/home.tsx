86f8f354e7a45f3f45db325a89291674e76c9ad8
Mangalam Raj <mango.26june@gmail.com>
2024-07-11 21:49:12 +0530

    bug fix

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,9 @@
 import AboutCarousel from "../components/about-carousel/aboutCarousel";
 import About from "../components/about/aboutSection";
-import CTA from "../components/cta/cta";
-import FeatureSection from "../components/features/featureSection";
+
 import Footer from "../components/footer/footer";
 import HeroSection from "../components/hero/heroSection";
 import Navbar from "../components/navbar";
-import Pricing from "../components/pricing/pricing";
 import MarqueeEffect from "../components/collab/collab";
 import AOS from "aos";
 import "aos/dist/aos.css";
